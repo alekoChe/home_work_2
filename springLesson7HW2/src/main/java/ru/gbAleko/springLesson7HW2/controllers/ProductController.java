751200c2f0eb7cd15660d3200b53fdceab8a25f1
alekoChe <aleko2117bis@gmail.com>
2022-05-30 20:55:42 +0300

    add ...

--- springLesson7HW2/src/main/java/ru/gbAleko/springLesson7HW2/controllers/ProductController.java
+++ springLesson7HW2/src/main/java/ru/gbAleko/springLesson7HW2/controllers/ProductController.java
@@ -1,5 +1,7 @@
 package ru.gbAleko.springLesson7HW2.controllers;
 
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.gbAleko.springLesson7HW2.data.Product;
 import ru.gbAleko.springLesson7HW2.services.ProductService;
@@ -20,6 +22,11 @@ public class ProductController {
         return productService.getAllProducts();
     }
 
+    @PostMapping("/products")
+    public ResponseEntity<?> addProduct(@RequestBody Product product) {
+        return productService.addProduct(product);
+    }
+
     @GetMapping("/products/find/{id}")
     public Product getById(@PathVariable Long id) {
         return productService.findById(id);
@@ -35,10 +42,13 @@ public class ProductController {
         productService.deleteById(id);
     }
 
-    @PostMapping("/products/add")
-    public void addProduct(@RequestParam String title, @RequestParam int cost) {
-        productService.addProduct(title, cost);
-    }
+    @GetMapping("/products/deleteById")
+    public void deleteProductById(@RequestParam Long id) { productService.deleteById(id);}
+
+//    @PostMapping("/products/add")
+//    public void addProduct(@RequestParam String title, @RequestParam int cost) {
+//        productService.addProduct(title, cost);
+//    }
 
     @GetMapping("products/between")
     public List<Product> findByRangeBetween(@RequestParam(defaultValue = "0") Integer min, @RequestParam(defaultValue = "100000") Integer max) {
@@ -53,5 +63,16 @@ public class ProductController {
     @GetMapping("/products/more")
     public  List<Product> getHighThenMax(@RequestParam(defaultValue = "0") Integer min) {
         return productService.findCostMoreThan(min);
+    }
+
+    @GetMapping("products/between_page")
+    public Page<Product> getAllStudents(@RequestParam(name = "p", defaultValue = "1") Integer page,
+                                        @RequestParam(name = "min_Cost", required = false) Integer minCost,
+                                        @RequestParam(name = "max_Cost", required = false) Integer maxCost
+    ) {
+        if(page < 1){
+            page = 1;
+        }
+        return productService.find(page, minCost, maxCost);
     }
 }
---
+++ springLesson7HW2/src/main/resources/static/index.js
@@ -0,0 +1,76 @@
+angular.module('app', []).controller('indexController', function ($scope, $http) {
+    const contextPath = 'http://localhost:8189/app';
+
+    $scope.loadProducts = function () {
+        $http.get(contextPath + '/products')
+            .then(function (response) {
+                console.log(response.data);
+                $scope.productList = response.data;
+            });
+    };
+
+    $scope.selectionProductInPriceRange = function (min, max) {
+        console.log($scope.productsBetween)
+        $http({
+            url: contextPath + "/products/between",
+            method: 'get',
+            params: {
+                min: $scope.productsBetween.min,
+                max: $scope.productsBetween.max
+            }
+        }).then(function (response){
+            //$scope.loadProducts();  // выведет весь список
+            $scope.productList = response.data;
+        });
+    }
+
+    $scope.addProductIntoCart = function (productId){
+            $http({
+                url: contextPath + '/cart/add',
+                method: 'GET',
+                params: {
+                    productId: productId
+                }
+            }).then(function (response){
+                $scope.loadCart();
+            });
+    };
+    $scope.deleteStudent = function (studentId) {
+            $http.get(contextPath + '/students/delete/' + studentId)
+                .then(function (response) {
+                    $scope.loadStudents();
+                });
+    }
+    $scope.deleteProduct = function () {
+            console.log($scope.deleteProductId)
+            $http.get(contextPath + '/products/delete/', $scope.deleteProductId)
+                .then(function (response) {
+                    $scope.loadStudents();
+                });
+    }
+
+//    $scope.deleteProduct = function (productId) {
+//            console.log($scope.deleteProductById)
+//            $http({
+//                url: contextPath + "/products/deleteById", ///////////////////////
+//                method: 'get',
+//                params: {
+//                    productId: $scope.deleteProductById.productId
+//                }
+//            }).then(function (response){
+//                $scope.loadProducts();  // выведет весь список
+//                //$scope.productList = response.data;
+//            });
+//    }
+
+    $scope.createProductJson = function (){
+            console.log($scope.newProductJson);
+            $http.post(contextPath + '/products', $scope.newProductJson)
+                .then(function (response) {
+                    $scope.loadProducts()
+                });
+    }
+
+    $scope.loadProducts();
+
+});
